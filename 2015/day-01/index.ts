472e54caaa182f2b6e08053bde86cd9b398c3d05
Marco Biedermann <marco.biedermann@gmx.de>
2019-12-19 09:41:28 +0100

    feat: 2015/01
    Add day 01 of 2015

---
+++ 2015/day-01/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync(`${__dirname}/input.txt`, 'utf-8');
+
+const parentheses = [...input];
+
+const floor = parentheses.reduce((accumulator, currentValue, index) => {
+  const floor = currentValue === '(' ? accumulator + 1 : accumulator - 1;
+
+  if (floor === -1) {
+    console.log('Enter Basement at position: ', index + 1);
+  }
+
+  return floor;
+}, 0);
+
+floor;

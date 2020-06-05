7cc3329af1e62172a3fdcd8ba13bcede7b96a1b9
Marco Biedermann <marco.biedermann@gmx.de>
2020-06-05 15:57:28 +0200

    test: 2019 Day 02
    Add tests

---
+++ 2019/day-02/index.test.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs';
+import { part1 } from '.';
+
+const input = readFileSync(`${__dirname}/input.txt`, 'utf-8');
+const data = input.split(',').map(Number);
+
+describe('day-01', () => {
+  describe('Part 1', () => {
+    it('should run instructions of operators', () => {
+      expect.assertions(1);
+
+      expect(part1(data)[0]).toStrictEqual(5866663);
+    });
+  });
+});
--- 2019/day-02/index.ts
+++ 2019/day-02/index.ts
@@ -1,27 +1,25 @@
-import { readFileSync } from 'fs';
+function part1(opcodes: number[]): number[] {
+  opcodes[1] = 12;
+  opcodes[2] = 2;
 
-const input = readFileSync(`${__dirname}/input.txt`, 'utf-8');
+  for (let i = 0; i < opcodes.length; i += 4) {
+    const opcode = opcodes[i];
+    const inputA = opcodes[opcodes[i + 1]];
+    const inputB = opcodes[opcodes[i + 2]];
+    const outputIndex = opcodes[i + 3];
 
-const opcodes = input.split(',').map(Number);
-
-opcodes[1] = 12;
-opcodes[2] = 2;
-
-for (let i = 0; i < opcodes.length; i += 4) {
-  const opcode = opcodes[i];
-  const inputA = opcodes[opcodes[i + 1]];
-  const inputB = opcodes[opcodes[i + 2]];
-  const outputIndex = opcodes[i + 3];
-
-  if (opcode === 1) {
-    opcodes[outputIndex] = inputA + inputB;
-  } else if (opcode === 2) {
-    opcodes[outputIndex] = inputA * inputB;
-  } else if (opcode === 99) {
-    break;
-  } else {
-    console.log('Invalid input');
+    if (opcode === 1) {
+      opcodes[outputIndex] = inputA + inputB;
+    } else if (opcode === 2) {
+      opcodes[outputIndex] = inputA * inputB;
+    } else if (opcode === 99) {
+      break;
+    } else {
+      console.log('Invalid input');
+    }
   }
+
+  return opcodes;
 }
 
-opcodes;
+export { part1 };

9ae38c686f81cee259e9b0612272a8540d13fde8
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-10 15:38:36 +0200

    feat: 2016 Day 4
    Add day 4 of 2016

---
+++ 2016/day/4/index.test.ts
@@ -0,0 +1,30 @@
+import { readFileSync } from 'fs';
+import { part1, part2 } from '.';
+
+const input = readFileSync(`${__dirname}/input`, 'utf-8').split('\n');
+
+describe('2016 Day 3', () => {
+  describe('Part 1', () => {
+    it('should sum the sector IDs of real rooms', () => {
+      expect.assertions(2);
+
+      expect(
+        part1([
+          'aaaaa-bbb-z-y-x-123[abxyz]',
+          'a-b-c-d-e-f-g-h-987[abcde]',
+          'not-a-real-room-404[oarel]',
+          'totally-real-room-200[decoy]',
+        ]),
+      ).toStrictEqual(1514);
+      expect(part1(input)).toStrictEqual(278221);
+    });
+  });
+
+  describe('Part 2', () => {
+    it('should find the sector ID of the room where the northpole objects are being stored', () => {
+      expect.assertions(1);
+
+      expect(part2(input)).toStrictEqual(267);
+    });
+  });
+});
--- README.md
+++ README.md
@@ -177,7 +177,7 @@ My attempt of solving [Advent of Code](https://adventofcode.com/) puzzles in Jav
 | [Day 7](2016/day/7)   |       |
 | [Day 6](2016/day/6)   |       |
 | [Day 5](2016/day/5)   |       |
-| [Day 4](2016/day/4)   |       |
+| [Day 4](2016/day/4)   | ⭐⭐  |
 | [Day 3](2016/day/3)   | ⭐    |
 | [Day 2](2016/day/2)   | ⭐⭐  |
 | [Day 1](2016/day/1)   | ⭐    |

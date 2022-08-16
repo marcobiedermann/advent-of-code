cdf1360ca5da83fd4e12d0436becdd2f7beab202
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-16 11:20:53 +0200

    feat: 2018 Day 3 Part 1
    Add part1 of day 3 in 2018

---
+++ 2018/day/3/index.test.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from 'node:fs';
+import { part1 } from '.';
+
+const input = readFileSync(`${__dirname}/input`, 'utf-8').split('\n');
+const inputSample = readFileSync(`${__dirname}/input.sample`, 'utf-8').split('\n');
+
+describe('Day 3', () => {
+  describe('Part 1', () => {
+    it('should return the number of overlapping claims', () => {
+      expect.assertions(2);
+
+      expect(part1(inputSample)).toStrictEqual(4);
+      expect(part1(input)).toStrictEqual(110546);
+    });
+  });
+});

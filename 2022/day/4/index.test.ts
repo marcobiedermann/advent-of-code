98c3af72d89e4a38c359f1ed4a1f65ab7835e931
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-12-04 12:43:45 +0100

    feat: 2022 Day 04
    Add part 1 and 2 of day 4 in 2022

---
+++ 2022/day/4/index.test.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'node:fs';
+import { part1, part2 } from '.';
+
+const sample = readFileSync(`${__dirname}/input.sample`, 'utf-8').split('\n');
+const input = readFileSync(`${__dirname}/input`, 'utf-8').split('\n');
+
+describe('Day 4', () => {
+  describe('Part 1', () => {
+    it('should find the number of fully contained assignment sections', () => {
+      expect.assertions(2);
+
+      expect(part1(sample)).toStrictEqual(2);
+      expect(part1(input)).toStrictEqual(511);
+    });
+  });
+
+  describe('Part 2', () => {
+    it('should find the number of overlapping assignment sections', () => {
+      expect.assertions(2);
+
+      expect(part2(sample)).toStrictEqual(4);
+      expect(part2(input)).toStrictEqual(821);
+    });
+  });
+});

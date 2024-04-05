0226812933fdefa8e3b3fff05b0f10dc97aec551
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-05 12:47:37 +0200

    refactor: 2015 Day 4
    Refactor day 4 of 2015

--- 2015/day/4/index.test.ts
+++ 2015/day/4/index.test.ts
@@ -3,20 +3,20 @@ import { part1, part2 } from '.';
 
 const input = 'ckczppom';
 
-describe('Day 4', () => {
-  describe('Part 1', () => {
-    it('should find a hash which starts with five zeros', () => {
-      expect.assertions(1);
-
-      expect(part1(input)).toStrictEqual(117946);
+describe('2015', () => {
+  describe('Day 4', () => {
+    describe('Part 1', () => {
+      it('should find a hash which starts with five zeros', () => {
+        expect(part1('abcdef')).toBe(609043);
+        expect(part1('pqrstuv')).toBe(1048970);
+        expect(part1(input)).toBe(117946);
+      });
     });
-  });
 
-  describe('Part 2', () => {
-    it('should find a hash which starts with six zeros', () => {
-      expect.assertions(1);
-
-      expect(part2(input)).toStrictEqual(3938038);
+    describe('Part 2', () => {
+      it('should find a hash which starts with six zeros', () => {
+        expect(part2(input)).toBe(3938038);
+      });
     });
   });
 });

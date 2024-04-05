b1135559542553fcc28e6590055c487405433715
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-05 13:49:20 +0200

    refactor: 2015 Day 5
    Refactor day 5 of 2015

--- 2015/day/5/index.test.ts
+++ 2015/day/5/index.test.ts
@@ -4,32 +4,30 @@ import { part1, part2 } from '.';
 
 const input = (await readFile(`${__dirname}/input`, 'utf-8')).split('\n');
 
-describe('2015 Day 5', () => {
-  describe('Part 1', () => {
-    it('should filter out nice words', () => {
-      expect.assertions(2);
-
-      expect(
-        part1([
-          'ugknbfddgicrmopn',
-          'aaa',
-          'jchzalrnumimnmhp',
-          'haegwjzuvuyypxyu',
-          'dvszwmarrgswjxmb',
-        ]),
-      ).toStrictEqual(2);
-      expect(part1(input)).toStrictEqual(255);
+describe('2015', () => {
+  describe('Day 5', () => {
+    describe('Part 1', () => {
+      it('should filter out nice words', () => {
+        expect(
+          part1([
+            'ugknbfddgicrmopn',
+            'aaa',
+            'jchzalrnumimnmhp',
+            'haegwjzuvuyypxyu',
+            'dvszwmarrgswjxmb',
+          ]),
+        ).toBe(2);
+        expect(part1(input)).toBe(255);
+      });
     });
-  });
 
-  describe('Part 2', () => {
-    it('should filter out nice words', () => {
-      expect.assertions(2);
-
-      expect(
-        part2(['qjhvhtzxzqqjkmpb', 'xxyxx', 'uurcxstgmygtbstg', 'ieodomkazucvgmuy']),
-      ).toStrictEqual(2);
-      expect(part2(input)).toStrictEqual(55);
+    describe('Part 2', () => {
+      it('should filter out nice words', () => {
+        expect(part2(['qjhvhtzxzqqjkmpb', 'xxyxx', 'uurcxstgmygtbstg', 'ieodomkazucvgmuy'])).toBe(
+          2,
+        );
+        expect(part2(input)).toBe(55);
+      });
     });
   });
 });
